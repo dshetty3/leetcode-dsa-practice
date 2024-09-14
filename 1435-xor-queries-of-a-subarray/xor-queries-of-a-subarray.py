c1d26c0fa3b404968957b0f7c78b5ed0cd8840fd
Disha Shetty <125394382+dshetty3@users.noreply.github.com>
2024-09-13 21:17:28 -0400

    Time: 623 ms (5.26%) | Memory: 26.6 MB (57.89%) - LeetSync

--- 1435-xor-queries-of-a-subarray/xor-queries-of-a-subarray.py
+++ 1435-xor-queries-of-a-subarray/xor-queries-of-a-subarray.py
@@ -5,12 +5,23 @@ class Solution(object):
         :type queries: List[List[int]]
         :rtype: List[int]
         """
-        prefix = [0] #initialize it by zero so that starts from 0, 1, 2, 3
+        # prefix = [0] #initialize it by zero so that starts from 0, 1, 2, 3
 
-        for n in arr:
-            prefix.append(prefix[-1] ^ n)
+        # for n in arr:
+        #     prefix.append(prefix[-1] ^ n)
+
+        # res = []
+        # for left, right in queries:
+        #     res.append(prefix[right + 1] ^ prefix[left]) 
+        # return res
+
+
+        for i in range(1, len(arr)):
+            arr[i] ^= arr[i-1]
 
         res = []
         for left, right in queries:
-            res.append(prefix[right + 1] ^ prefix[left]) 
+            total = arr[right]
+            remove = 0 if left == 0 else arr[left - 1]
+            res.append( total ^ remove )
         return res
