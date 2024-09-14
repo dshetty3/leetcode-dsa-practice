e8e91dc142fbd1a1884a13189fee6fed60eade89
Disha Shetty <125394382+dshetty3@users.noreply.github.com>
2024-09-14 16:03:31 -0400

    Time: 1430 ms (5.55%) | Memory: 24.4 MB (50.00%) - LeetSync

--- 2503-longest-subarray-with-maximum-bitwise-and/longest-subarray-with-maximum-bitwise-and.py
+++ 2503-longest-subarray-with-maximum-bitwise-and/longest-subarray-with-maximum-bitwise-and.py
@@ -4,14 +4,35 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        #bitwise AND of same number will be same
-        # Bitwiase and of smalled number and bigger will lead to smaller than the bigger number i.e. 2 & 3 = 2
-        target = max(nums)
+        # if n and curr_max equal, n & curr_max = curr_max
+        # if n < curr_max, n & curr_max < curr_max
+        # if n > curr_max, n & curr_max < curr_max
+
+
+
+        # target = max(nums)
+        # size, res = 0, 0
+        # for n in nums:
+        #     if n == target:
+        #         size += 1
+        #     else:
+        #         size = 0
+        #     res = max(res, size)
+        # return res
+
+
         size, res = 0, 0
+        curr_max = 0
+        res = 0
         for n in nums:
-            if n == target:
+            if n > curr_max:
+                curr_max = n
+                size = 1
+                res = 0
+            elif n == curr_max:
                 size += 1
             else:
-                size = 0
-            res = max(res, size)
+                size = 0 
+            res = max(size, res)       
         return res
+
