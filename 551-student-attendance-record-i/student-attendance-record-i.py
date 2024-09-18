e26265d98636ec648becc9cf8dbabba2d6bb60fe
Disha Shetty <125394382+dshetty3@users.noreply.github.com>
2024-09-18 00:12:18 -0400

    Time: 18 ms (25.64%) | Memory: 11.7 MB (20.79%) - LeetSync

--- 551-student-attendance-record-i/student-attendance-record-i.py
+++ 551-student-attendance-record-i/student-attendance-record-i.py
@@ -5,9 +5,24 @@ class Solution(object):
         :rtype: bool
         """
 
-        if s.count('A') >= 2:
-            return False
-        if 'LLL' in s:
-            return False
+        # if s.count('A') >= 2:
+        #     return False
+        # if 'LLL' in s:
+        #     return False
+        # return True
+
+        countA = 0
+        countL = 0
+
+        for i in s:
+            if i == 'A':
+                countA += 1
+            if i == 'L': 
+                countL += 1
+            else:
+                countL = 0
+            
+            if countA >= 2 or countL >= 3:
+                return False
         return True
         
