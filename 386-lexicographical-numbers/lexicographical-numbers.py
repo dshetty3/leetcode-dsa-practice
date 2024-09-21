f8714e4f99e4090fd46759024229c9eb63b007b0
Disha Shetty <125394382+dshetty3@users.noreply.github.com>
2024-09-21 13:20:05 -0400

    Time: 69 ms (71.31%) | Memory: 16.8 MB (87.70%) - LeetSync

---
+++ 386-lexicographical-numbers/lexicographical-numbers.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def lexicalOrder(self, n):
+        """
+        :type n: int
+        :rtype: List[int]
+        """
+        res = []
+        curr = 1
+
+        while len(res) < n:
+            res.append(curr)
+
+            if curr * 10 <= n:
+                curr *= 10
+            else:
+                while curr == n or curr % 10 == 9:
+                    curr = curr // 10
+                curr += 1
+
+        return res
