24038c6897c69930a159c4a069b8c477b8b6a7c1
Disha Shetty <125394382+dshetty3@users.noreply.github.com>
2024-09-23 16:37:42 -0400

    Time: 131 ms (21.56%) | Memory: 15.1 MB (62.05%) - LeetSync

---
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def topKFrequent(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: List[int]
+        """
+
+        count = Counter(nums)
+        heap = []
+
+        for key, val in count.items():
+            if len(heap) < k:
+                heapq.heappush(heap, (val, key))
+            else:
+                heapq.heappushpop(heap, (val,key))
+
+        return [h[1] for h in heap]
+
+
+        
