55b09e985cbd9c102646f03258d2219a7c1004ca
Disha Shetty <125394382+dshetty3@users.noreply.github.com>
2024-09-30 11:05:48 -0400

    Time: 53 ms (43.76%) | Memory: 11.8 MB (88.78%) - LeetSync

---
+++ 2163-kth-distinct-string-in-an-array/kth-distinct-string-in-an-array.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def kthDistinct(self, arr, k):
+        """
+        :type arr: List[str]
+        :type k: int
+        :rtype: str
+        """
+
+        count = {}
+
+        for s in arr:
+            if s not in count:
+                count[s] = 0
+            count[s] += 1
+        
+        for s in arr:
+            if count[s] == 1:
+                k -= 1
+            if k == 0:
+                return s
+        return ""
+
+        
