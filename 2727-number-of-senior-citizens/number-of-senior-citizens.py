573e7e5aee518848d0791120d961bc960e9e4f20
Disha Shetty <125394382+dshetty3@users.noreply.github.com>
2024-09-30 17:04:45 -0400

    Time: 26 ms (46.41%) | Memory: 11.7 MB (82.46%) - LeetSync

--- 2727-number-of-senior-citizens/number-of-senior-citizens.py
+++ 2727-number-of-senior-citizens/number-of-senior-citizens.py
@@ -7,7 +7,7 @@ class Solution(object):
 
         count = 0
         for detail in details:
-            age = int(detail[-4:-2])
+            age = int(detail[11:13])
             if age > 60:
                 count += 1
         return count
