e7263a3c97278b72530d79e07ebf398fc063e515
Disha Shetty <125394382+dshetty3@users.noreply.github.com>
2024-10-03 19:26:00 -0400

    Time: 398 ms (87.81%) | Memory: 28 MB (95.12%) - LeetSync

---
+++ 1694-make-sum-divisible-by-p/make-sum-divisible-by-p.py
@@ -0,0 +1,31 @@
+class Solution(object):
+    def minSubarray(self, nums, p):
+        """
+        :type nums: List[int]
+        :type p: int
+        :rtype: int
+        """
+
+        total = sum(nums)
+        remain = total % p
+
+        if remain == 0:
+            return 0
+
+        res = len(nums)
+        remain_index ={
+            0: -1
+        }
+
+        cur_sum = 0
+        for i, n in enumerate(nums):
+            cur_sum = (cur_sum + n) % p
+            prefix = (cur_sum - remain + p) % p
+            if prefix in remain_index:
+                length = i - remain_index[prefix]
+                res = min(res, length)
+            remain_index[cur_sum] = i
+
+
+
+        return -1 if res == len(nums) else res        
