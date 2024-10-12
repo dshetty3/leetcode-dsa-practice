d1d3c48727f91ab8450b2d30881d1b5794b84d8b
Disha Shetty <125394382+dshetty3@users.noreply.github.com>
2024-10-12 01:05:18 -0400

    Time: 959 ms (94.35%) | Memory: 54.3 MB (83.05%) - LeetSync

---
+++ 2488-divide-intervals-into-minimum-number-of-groups/divide-intervals-into-minimum-number-of-groups.py
@@ -0,0 +1,29 @@
+class Solution(object):
+    def minGroups(self, intervals):
+        """
+        :type intervals: List[List[int]]
+        :rtype: int
+        """
+
+        start, end = [], []
+        
+        for l, r in intervals:
+            start.append(l)
+            end.append(r)
+
+        start.sort()
+        end.sort()
+
+        i, j = 0,0
+        res, groups = 0, 0
+        while i < len(intervals):
+            if start[i] <= end[j]:
+                groups += 1
+                i += 1
+            else:
+                groups -= 1
+                j += 1
+            res = max(res, groups)
+        return res
+        
+        
