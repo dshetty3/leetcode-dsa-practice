05df2ba268b917f409e773c2105ac25ada338412
Disha Shetty <125394382+dshetty3@users.noreply.github.com>
2024-10-20 17:14:03 -0400

    Time: 51 ms (94.68%) | Memory: 12.5 MB (36.46%) - LeetSync

---
+++ 680-valid-palindrome-ii/valid-palindrome-ii.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def validPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+
+        l = 0
+        r = len(s) - 1
+        while l < r:
+            if s[l] != s[r]:
+                skipL, skipR = s[l + 1: r + 1], s[l:r]
+                return (skipL == skipL[::-1] or skipR == skipR[::-1])
+            l += 1
+            r -= 1
+        return True
+        
