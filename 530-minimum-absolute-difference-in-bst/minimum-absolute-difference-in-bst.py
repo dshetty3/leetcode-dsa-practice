4cdf8b361121a3eb42e3fab8e12130720398406e
Disha Shetty <125394382+dshetty3@users.noreply.github.com>
2024-10-27 19:47:36 -0400

    Time: 3 ms (77.90%) | Memory: 18.9 MB (6.87%) - LeetSync

--- 530-minimum-absolute-difference-in-bst/minimum-absolute-difference-in-bst.py
+++ 530-minimum-absolute-difference-in-bst/minimum-absolute-difference-in-bst.py
@@ -1,33 +1,27 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
+# class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution(object):
-    def getMinimumDifference(self, root):
-        """
-        :type root: Optional[TreeNode]
-        :rtype: int
-        """
-
-        min_distance = [float('inf')]
-        prev = [None]
+class Solution:
+    def getMinimumDifference(self, root: Optional[TreeNode]) -> int:
+        min_distance = float('inf')
+        prev = None
 
         def dfs(node):
+            nonlocal min_distance, prev
             if node is None:
                 return
             
             dfs(node.left)
 
-            if prev[0] is not None:
-                min_distance[0] = min(min_distance[0], node.val - prev[0])
+            if prev is not None:
+                min_distance = min(min_distance, node.val - prev)
             
-            prev[0] = node.val
+            prev = node.val
             dfs(node.right)
 
         dfs(root)
-        return min_distance[0]
-            
-
+        return min_distance
         
