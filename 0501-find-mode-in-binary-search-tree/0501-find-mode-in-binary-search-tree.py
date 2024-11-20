80d2205b40707b2e9ad680976130a0d7de0ffe59
Disha Shetty <125394382+dshetty3@users.noreply.github.com>
2024-11-19 20:54:27 -0500

    Time: 7 ms (81.01%), Space: 19.5 MB (59.03%) - LeetHub

---
+++ 0501-find-mode-in-binary-search-tree/0501-find-mode-in-binary-search-tree.py
@@ -0,0 +1,34 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def findMode(self, root):
+        """
+        :type root: Optional[TreeNode]
+        :rtype: List[int]
+        """
+        def dfs(node, counter):
+            if not node:
+                return
+            
+            counter[node.val] += 1
+            dfs(node.left, counter)
+            dfs(node.right, counter)
+
+        counter = defaultdict(int)
+        dfs(root, counter)
+        max_freq = max(counter.values())
+
+        res = []
+        for key in counter:
+            if counter[key] == max_freq:
+                res.append(key)
+        return res
+
+        
+        
+
+        
