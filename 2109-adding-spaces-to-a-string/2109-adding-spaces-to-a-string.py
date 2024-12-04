c113b50b508e0482e54b63be744313c06df35c56
Disha Shetty <125394382+dshetty3@users.noreply.github.com>
2024-12-04 12:02:38 -0500

    Time: 255 ms (7.14%), Space: 37.4 MB (100%) - LeetHub

--- 2109-adding-spaces-to-a-string/2109-adding-spaces-to-a-string.py
+++ 2109-adding-spaces-to-a-string/2109-adding-spaces-to-a-string.py
@@ -5,13 +5,24 @@ class Solution(object):
         :type spaces: List[int]
         :rtype: str
         """
+        i, j = 0, 0
         res = []
 
-        res.append(s[:spaces[0]])
-        for i in range(1,len(spaces)):
-            res.append(s[spaces[i - 1]:spaces[i]])
-        res.append(s[spaces[-1]:])
+        # res.append(s[:spaces[0]])
+        # for i in range(1,len(spaces)):
+        #     res.append(s[spaces[i - 1]:spaces[i]])
+        # res.append(s[spaces[-1]:])
 
-        return ' '.join(res)
+        while i < len(s) and j < len(spaces):
+            if i < spaces[j]:
+                res.append(s[i])
+                i += 1
+            else:
+                res.append(" ")
+                j += 1
+        if i < len(s):
+            res.append(s[i:])
+
+        return "".join(res)
 
         
