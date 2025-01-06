02470951581420bfbb96e142fb790a327e45648e
Disha Shetty <125394382+dshetty3@users.noreply.github.com>
2025-01-05 23:29:07 -0500

    Time: 0 ms (100%), Space: 17.9 MB (20.76%) - LeetHub

---
+++ 0078-subsets/0078-subsets.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def subsets(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        res = []
+
+        subset = []
+        def dfs(i):
+            if i >= len(nums):
+                res.append(subset.copy())
+                return
+
+            subset.append(nums[i])
+            dfs(i + 1)
+
+            subset.pop()
+            dfs(i + 1)
+        dfs(0)
+        return res
+
+        
+        
