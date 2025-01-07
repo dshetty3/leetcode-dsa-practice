e650dac86bf62efce84f0be91517f7166bf1b116
Disha Shetty <125394382+dshetty3@users.noreply.github.com>
2025-01-07 01:44:54 -0500

    Time: 9 ms (36.4%), Space: 19.4 MB (17.71%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.py
@@ -0,0 +1,21 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+
+        count = {}
+
+        for n in nums:
+            if n not in count:
+                count[n] = 1
+            else:
+                count[n] += 1
+        
+
+        max_val = -1
+        ans = -1
+
+        for key,val in count.items():
+            if val > max_val:
+                max_val = val
+                ans = key
+        return ans
+        
