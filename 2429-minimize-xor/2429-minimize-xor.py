9073526aa1f0dd0e3df59fab52f9f71629331a3c
Disha Shetty <125394382+dshetty3@users.noreply.github.com>
2025-01-15 18:17:10 -0500

    Time: 0 ms (100%), Space: 17.7 MB (59.52%) - LeetHub

---
+++ 2429-minimize-xor/2429-minimize-xor.py
@@ -0,0 +1,23 @@
+class Solution:
+    def minimizeXor(self, num1: int, num2: int) -> int:
+        def count_bits(n):
+            res = 0
+            while n > 0:
+                n  = n & (n - 1)
+                res += 1
+            return res
+        
+        cnt1 = count_bits(num1)
+        cnt2 = count_bits(num2)
+        x = num1
+        i = 0
+
+        while cnt1 != cnt2:
+            if cnt2 < cnt1 and x & (1 << i):
+                cnt1 -= 1
+                x = x ^ (1 << i)
+            if cnt1 < cnt2 and x & (1 << i) == 0:
+                cnt1 += 1
+                x = x | (1 << i)
+            i += 1
+        return x
