e320e385b0b9e73408991ddd0f5fa4d6ce0743e1
Disha Shetty <125394382+dshetty3@users.noreply.github.com>
2025-01-26 23:52:45 -0800

    Time: 59 ms (15.68%), Space: 48.8 MB (74.33%) - LeetHub

---
+++ 2635-apply-transform-over-each-element-in-array/2635-apply-transform-over-each-element-in-array.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+var map = function(arr, fn) {
+    let res = [];
+
+    for (const i in arr){
+        res.push(fn(arr[i], Number(i)));
+    }
+    
+    return res;
+    
+};
