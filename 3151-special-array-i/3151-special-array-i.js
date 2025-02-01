90f731c86a43c6aa4bcdd9d2058c772c375478d9
Disha Shetty <125394382+dshetty3@users.noreply.github.com>
2025-01-31 16:33:08 -0800

    Time: 0 ms (100%), Space: 51.9 MB (18.6%) - LeetHub

---
+++ 3151-special-array-i/3151-special-array-i.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var isArraySpecial = function(nums) {
+
+    if (nums.length < 2) {
+         return true
+    }
+
+    for(let i = 1; i < nums.length; i++){
+            if ((nums[i] % 2) === (nums[i -1] % 2)) {
+                return false     
+    }
+    }
+    return true;  
+};
