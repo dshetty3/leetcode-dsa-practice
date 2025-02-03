c5fe7fce1e09181137acc72805a5d118c093bc1a
Disha Shetty <125394382+dshetty3@users.noreply.github.com>
2025-02-02 23:56:18 -0800

    Time: 1 ms (83.93%), Space: 51.4 MB (57.14%) - LeetHub

--- 3105-longest-strictly-increasing-or-strictly-decreasing-subarray/3105-longest-strictly-increasing-or-strictly-decreasing-subarray.js
+++ 3105-longest-strictly-increasing-or-strictly-decreasing-subarray/3105-longest-strictly-increasing-or-strictly-decreasing-subarray.js
@@ -3,33 +3,33 @@
  * @return {number}
  */
 var longestMonotonicSubarray = function(nums) {
-    if (nums.length === 0) return 0;
+    
+    let cur = 1
+    let res = 1
+    let increasing = 0
 
-    let cur = 1;
-    let res = 1;
-    let increase = 0;
-
-    for (let i = 1; i < nums.length; i++) {
-        if (nums[i - 1] < nums[i]) {
-            if (increase > 0) {
-                cur += 1;
-            } else {
-                cur = 2;
-                increase = 1;
+    for(let i = 1; i < nums.length; i++){
+        if (nums[i-1] < nums[i]){
+            if (increasing > 0){
+                cur += 1 
+            } else{
+                cur = 2
+                increasing = 1
             }
-        } else if (nums[i - 1] > nums[i]) {
-            if (increase < 0) {
-                cur += 1;
+        } else if (nums[i-1] > nums[i]){
+            if (increasing < 0){
+                cur += 1
             } else {
-                cur = 2;
-                increase = -1;
+                cur = 2
+                increasing = -1
             }
         } else {
-            cur = 1;
-            increase = 0;
+            cur = 1
+            increasing = 1
         }
-        res = Math.max(res, cur);
-    }
 
-    return res;
+        res = Math.max(res, cur)
+    }
+    return res
+
 };
