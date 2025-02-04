5e72bdeb62f553fa1617d94e38b6d9cb2cb46163
Disha Shetty <125394382+dshetty3@users.noreply.github.com>
2025-02-03 22:49:06 -0800

    Time: 50 ms (68.17%), Space: 49.2 MB (81.45%) - LeetHub

---
+++ 2727-is-object-empty/2727-is-object-empty.js
@@ -0,0 +1,12 @@
+/**
+ * @param {Object|Array} obj
+ * @return {boolean}
+ */
+var isEmpty = function(obj) {
+
+    if(obj && Object.keys(obj).length === 0){
+        return true
+    }else return false
+    
+    
+};
