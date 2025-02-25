1486684fba306d7e3713b6b744b6b74b045c11f1
Disha Shetty <125394382+dshetty3@users.noreply.github.com>
2025-02-25 17:21:26 -0500

    Time: 4 ms (67.77%), Space: 12.4 MB (71.94%) - LeetHub

---
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def maxNumberOfBalloons(self, text):
+        """
+        :type text: str
+        :rtype: int
+        """
+
+        counter = defaultdict(int)
+
+        word = 'balloon'
+
+        for c in text:
+            if c in word:
+                counter[c] += 1
+            
+        for c in word:
+            if c not in counter:
+                return 0
+            
+        else:
+            return min(counter['b'], 
+                            counter['a'],
+                            counter['l'] // 2,
+                            counter['o'] // 2,
+                            counter['n'])
+        
