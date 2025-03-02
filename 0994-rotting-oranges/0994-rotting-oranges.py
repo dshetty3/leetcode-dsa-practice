266bd581e63230bb388b7706434c70ae5a2799fe
Disha Shetty <125394382+dshetty3@users.noreply.github.com>
2025-03-02 17:22:26 -0500

    Time: 4 ms (78.94%), Space: 12.6 MB (14.38%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.py
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -5,31 +5,33 @@ class Solution(object):
         :rtype: int
         """
 
-        q = collections.deque()
-        fresh = 0
-        time = 0
+        q = deque()
+        time, fresh = 0, 0
 
-        for r in range(len(grid)):
-            for c in range(len(grid[0])):
+        ROWS = len(grid)
+        COLS = len(grid[0])
+
+        for r in range(ROWS):
+            for c in range(COLS):
                 if grid[r][c] == 1:
                     fresh += 1
                 if grid[r][c] == 2:
-                    q.append((r, c))
+                    q.append((r,c))
 
-        directions = [[0, 1], [0, -1], [1, 0], [-1, 0]]
-        while fresh > 0 and q:
-            length = len(q)
-            for i in range(length):
+        directions = [[0,1], [0,-1], [1, 0], [-1,0]]
+        while q and fresh > 0:
+            for i in range(len(q)):
                 r, c = q.popleft()
 
                 for dr, dc in directions:
-                    row, col = r + dr, c + dc
-                    if (row in range(len(grid))
-                        and col in range(len(grid[0]))
-                        and grid[row][col] == 1
-                    ):
-                        grid[row][col] = 2
-                        q.append((row, col))
-                        fresh -= 1
+                    row = r + dr 
+                    col = c + dc
+                    if (row < 0 or row == ROWS or 
+                    col < 0 or col == COLS or 
+                    grid[row][col] != 1):
+                        continue
+                    grid[row][col] = 2
+                    q.append((row, col))
+                    fresh -= 1
             time += 1
         return time if fresh == 0 else -1            
