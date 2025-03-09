5faf8121bcfa998ab9ec2da1991c4fcbc74a724a
Disha Shetty <125394382+dshetty3@users.noreply.github.com>
2025-03-09 14:33:41 -0700

    Time: 0 ms (100%), Space: 12.4 MB (64.03%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.py
+++ 0063-unique-paths-ii/0063-unique-paths-ii.py
@@ -16,6 +16,6 @@ class Solution(object):
                 if not obstacleGrid[i - 1][j - 1]:
                     dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
         
-        return dp[-1][-1]
+        return dp[m][n]
                 
         
