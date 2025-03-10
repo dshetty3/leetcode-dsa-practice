2bd24caf5cec48ea52c3cdac20d157c328eeb0b6
Disha Shetty <125394382+dshetty3@users.noreply.github.com>
2025-03-09 22:24:32 -0700

    Time: 2883 ms (100%), Space: 20.2 MB (25%) - LeetHub

---
+++ 3306-count-of-substrings-containing-every-vowel-and-k-consonants-ii/3306-count-of-substrings-containing-every-vowel-and-k-consonants-ii.py
@@ -0,0 +1,33 @@
+class Solution(object):
+    def countOfSubstrings(self, word, k):
+        """
+        :type word: str
+        :type k: int
+        :rtype: int
+        """
+
+
+        def atleastk(k):
+
+            vowel = defaultdict(int)
+            non_vowel = 0
+            res = 0
+            l = 0
+
+            for r in range(len(word)):
+                if word[r] in 'aeiou':
+                    vowel[word[r]] += 1
+                else:
+                    non_vowel += 1
+                while len(vowel) == 5 and non_vowel > k:
+                    res += (len(word) - r)
+                    if word[l] in 'aeiou':
+                        vowel[word[l]] -= 1
+                    else:
+                        non_vowel -= 1
+                    if vowel[word[l]] == 0:
+                        vowel.pop(word[l])
+                    l += 1
+            return res
+        return atleastk(k - 1) - atleastk(k)
+        
