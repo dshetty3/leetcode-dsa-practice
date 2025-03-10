82fe07208e247edc1e48b1a689e04231a7ac79c4
Disha Shetty <125394382+dshetty3@users.noreply.github.com>
2025-03-10 13:32:43 -0700

    Time: 9 ms (35.07%), Space: 16 MB (17.33%) - LeetHub

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -0,0 +1,44 @@
+class Solution(object):
+    def findOrder(self, numCourses, prerequisites):
+        """
+        :type numCourses: int
+        :type prerequisites: List[List[int]]
+        :rtype: List[int]
+        """
+
+        # TC - O(E + V)
+        # SP - O(E + V)
+
+        order = []
+        g = defaultdict(list)
+        courses = prerequisites
+        for a, b in courses:
+            g[a].append(b)
+        
+
+        UNSEEN, VISITING, VISITED = 0, 1, 2
+        states = [UNSEEN] * numCourses
+
+        def dfs(i):
+            if states[i] == VISITED: 
+                return True
+            elif states[i] == VISITING: 
+                return False
+            states[i] = VISITING
+
+            for nei in g[i]:
+                if not dfs(nei): return False
+
+            states[i] = VISITED
+            order.append(i)
+            return True
+
+        for i in range(numCourses):
+            if not dfs(i): 
+                return []
+                
+        return order
+
+
+
+        
