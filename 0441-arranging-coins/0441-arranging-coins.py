d9fec7dbba59a2156b5ffcc93edac37a498cfc66
Disha Shetty <125394382+dshetty3@users.noreply.github.com>
2025-03-11 13:44:08 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0441-arranging-coins/0441-arranging-coins.py
+++ 0441-arranging-coins/0441-arranging-coins.py
@@ -5,15 +5,29 @@ class Solution(object):
         :rtype: int
         """
 
-        count = 0
+        # count = 0
 
-        i = 1
-        while n >= 0:
-            n -= i
-            if n >= 0: 
-                count += 1
-            i += 1
-        return count
+        # i = 1
+        # while n >= 0:
+        #     n -= i
+        #     if n >= 0: 
+        #         count += 1
+        #     i += 1
+        # return count
 
+
+        l = 1
+        r = n
+        res = 0
+
+        while l <= r:
+            m = (l + r) // 2
+            coins = (m / 2) * (m + 1)
+            if coins > n:
+                r = m - 1
+            else:
+                l = m + 1
+                res = max(res, m)
+        return res
         
         
