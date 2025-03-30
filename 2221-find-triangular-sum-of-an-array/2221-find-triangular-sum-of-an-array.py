ccfd521b0ff17f0e894c677d437667d7078ffa4f
Disha Shetty <125394382+dshetty3@users.noreply.github.com>
2025-03-30 00:17:29 -0700

    Time: 1516 ms (89.96%), Space: 12.6 MB (79.21%) - LeetHub

---
+++ 2221-find-triangular-sum-of-an-array/2221-find-triangular-sum-of-an-array.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def triangularSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        n = len(nums) 
+
+        if n == 0: return 0
+
+
+        while n > 0:
+            for i in range(n - 1):
+                nums[i] = (nums[i] + nums[i + 1]) % 10
+            n -= 1
+
+        return nums[0]
+
+        
