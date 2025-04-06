4ec72527a5527e2dac5cfc3b013b84d0eac831b0
Disha Shetty <125394382+dshetty3@users.noreply.github.com>
2025-04-06 11:27:14 -0700

    Time: 8 ms (32.42%), Space: 12.4 MB (83.79%) - LeetHub

---
+++ 0093-restore-ip-addresses/0093-restore-ip-addresses.py
@@ -0,0 +1,31 @@
+class Solution(object):
+    def restoreIpAddresses(self, s):
+        """
+        :type s: str
+        :rtype: List[str]
+        """
+
+        res = []
+
+        if len(s) > 12:
+            return []
+        
+
+        def backtrack(i, dots, currIP):
+            if dots == 4 and i == len(s):
+                res.append(currIP[:-1])
+                return
+            
+            if dots > 4:
+                return
+            
+            for j in range(i, min(i + 3, len(s))):
+                if int(s[i: j + 1]) < 256 and (i == j or s[i] != "0"):
+                    backtrack(j + 1, dots + 1, currIP + s[i: j + 1] + '.')
+        
+        backtrack(0, 0, "")
+        return res
+
+
+
+        
