9664c512a64ae0d5bf1c31c0c7b9a8599b247184
Disha Shetty <125394382+dshetty3@users.noreply.github.com>
2025-04-06 12:27:29 -0700

    Time: 299 ms (19.12%), Space: 14.1 MB (10.29%) - LeetHub

---
+++ 0368-largest-divisible-subset/0368-largest-divisible-subset.py
@@ -0,0 +1,34 @@
+class Solution(object):
+    def largestDivisibleSubset(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+
+        nums.sort()
+        cache = {}
+
+
+        def dfs(i):
+            if i == len(nums):
+                return []
+            if i in cache:
+                return cache[i]
+            
+            res = [nums[i]]
+            for j in range(i + 1, len(nums)):
+                if nums[j] % nums[i] == 0:
+                    temp = [nums[i]] + dfs(j)
+                    if len(temp) > len(res):
+                        res = temp
+            cache[i] = res
+            return res
+        
+        res = []
+        for i in range(len(nums)):
+            temp = dfs(i)
+            if len(temp) > len(res):
+                res = temp
+        return res
+
+        
