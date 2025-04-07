26c04e676b6ffea286b1afdeb6106690d4307ea9
Disha Shetty <125394382+dshetty3@users.noreply.github.com>
2025-04-07 13:02:09 -0700

    Time: 0 ms (100%), Space: 17.9 MB (33.04%) - LeetHub

--- 1732-find-the-highest-altitude/1732-find-the-highest-altitude.py
+++ 1732-find-the-highest-altitude/1732-find-the-highest-altitude.py
@@ -9,12 +9,20 @@ class Solution:
         #     highest = max(highest, current)
         # return highest
 
-        gain1 = [0] 
-        height = 0
+        # gain1 = [0] 
+        # height = 0
+        # for g in gain:
+        #     height += g
+        #     gain1.append(height)
+        # return max(gain1)
+
+        current_altitude = 0
+        highest = current_altitude
+
         for g in gain:
-            height += g
-            gain1.append(height)
-        return max(gain1)
+            current_altitude += g
+            highest = max(highest, current_altitude)
+        return highest
 
 
 
