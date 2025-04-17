99944aa4cab84b1feea9b97e8bafc7499ebd444b
Disha Shetty <125394382+dshetty3@users.noreply.github.com>
2025-04-17 12:47:46 -0500

    Time: 32 ms (97.75%), Space: 16.6 MB (69.06%) - LeetHub

--- 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.py
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.py
@@ -6,12 +6,24 @@ class Solution(object):
         :rtype: int
         """
 
-        pr = 0
-        hold = -prices[0]
+        # pr = 0
+        # hold = -prices[0]
 
-        for price in prices[1:]:
-            hold = max(hold, pr - price)
-            pr = max(pr, hold + price - fee)
-        return pr
+        # for price in prices[1:]:
+        #     hold = max(hold, pr - price)
+        #     pr = max(pr, hold + price - fee)
+        # return pr
             
+        if len(prices) < 2:
+            return 0
+        
+        profit = 0
+        minimum = prices[0]
+        for i in range(1, len(prices)):
+            if prices[i] < minimum:
+                minimum = prices[i]
+            elif prices[i] > minimum + fee:
+                profit += prices[i] - minimum - fee
+                minimum = prices[i] - fee
+        return profit
         
