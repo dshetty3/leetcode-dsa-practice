0a1f387bd9a63435febb47950cb270f091589572
Disha Shetty <125394382+dshetty3@users.noreply.github.com>
2025-04-29 09:13:11 -0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0032-longest-valid-parentheses/0032-longest-valid-parentheses.py
+++ 0032-longest-valid-parentheses/0032-longest-valid-parentheses.py
@@ -1,20 +1,18 @@
-class Solution(object):
-    def longestValidParentheses(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
+class Solution:
+    def longestValidParentheses(self, s: str) -> int:
 
-        stack = [-1]
 
         count = 0
-        for i in range(len(s)):
-            if s[i] == '(':
-                stack.append(i)
-            elif len(stack) == 1:
-                stack[0] = i
-            else:
+
+        stack = []
+
+        for c in s:
+            if c == '(':
+                stack.append(c)
+            elif c == ')' and stack:
                 stack.pop()
-                count = max(count, i - stack[-1])
+                count += 2
+            elif len(stack) == 1:
+                count = 0
         return count
         
