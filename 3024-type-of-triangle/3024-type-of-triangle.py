2e965d0dca628a6c764344ab38213c66f4d815dc
Disha Shetty <125394382+dshetty3@users.noreply.github.com>
2025-05-18 23:52:03 -0500

    Time: 0 ms (100%), Space: 12.5 MB (49.64%) - LeetHub

--- 3024-type-of-triangle/3024-type-of-triangle.py
+++ 3024-type-of-triangle/3024-type-of-triangle.py
@@ -5,14 +5,25 @@ class Solution(object):
         :rtype: str
         """
 
-        nums.sort()
+        # nums.sort()
 
-        if nums[0] + nums[1] <= nums[2]:
+        # if nums[0] + nums[1] <= nums[2]:
+        #     return "none"
+        # elif nums[0] == nums[2]:
+        #     return "equilateral"
+        # elif nums[0] == nums[1] or nums[1] == nums[2]:
+        #     return "isosceles"
+        # else:
+        #     return "scalene"
+
+        if nums[0] + nums[1] <= nums[2] or nums[0] + nums[2] <= nums[1] or nums[1] + nums[2] <= nums[0]:
             return "none"
-        elif nums[0] == nums[2]:
+
+        if nums[0] == nums[1] == nums[2]:
             return "equilateral"
-        elif nums[0] == nums[1] or nums[1] == nums[2]:
-            return "isosceles"
-        else:
+        elif nums[0] != nums[1] and nums[1] != nums[2] and nums[2] != nums[0]:
             return "scalene"
+        elif nums[0] == nums[1] or nums[1] == nums[2] or nums[2] == nums[0]:
+            return "isosceles"
+        
         
