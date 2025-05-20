4ad15e87e5c3e3213b3da80eadfd566a4b3733ea
Disha Shetty <125394382+dshetty3@users.noreply.github.com>
2025-05-20 01:34:39 -0500

    Time: 4390 ms (18.87%), Space: 17.7 MB (79.57%) - LeetHub

--- 1219-path-with-maximum-gold/1219-path-with-maximum-gold.py
+++ 1219-path-with-maximum-gold/1219-path-with-maximum-gold.py
@@ -1,27 +1,31 @@
-class Solution(object):
-    def getMaximumGold(self, grid):
-        """
-        :type grid: List[List[int]]
-        :rtype: int
-        """
+class Solution:
+    def getMaximumGold(self, grid: List[List[int]]) -> int:
 
         ROWS = len(grid)
         COLS = len(grid[0])
-
-        def dfs(r, c, visit):
-            if (r < 0 or c < 0 or r == ROWS or c == COLS or grid[r][c] == 0 or(r, c) in visit):
-                return 0
-            visit.add((r,c))
-            res = grid[r][c]
-            directions = [(r + 1, c), (r - 1, c), (r, c - 1), (r, c + 1)]
-            for nr, nc in directions:
-                res = max(res, grid[r][c] + dfs(nr, nc, visit))
-            visit.remove((r,c))
-            return res
-
+        visit = set()
+        directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
         res = 0
+
+
+        def dfs(r, c):
+            if (r < 0 or c < 0 or r >= ROWS or c >= COLS or (r, c) in visit or grid[r][c] == 0):
+                return 0
+            visit.add((r, c))
+            res = grid[r][c]
+            for dr, dc in directions:
+                row = r + dr
+                col = c + dc
+                res = max(res, grid[r][c] + dfs(row, col))
+            visit.remove((r, c))
+            return res
+        
         for r in range(ROWS):
             for c in range(COLS):
-                res = max(res, dfs(r, c, set()))
+                res = max(res, dfs(r, c))
         return res
+                
+            
+                    
+
         
