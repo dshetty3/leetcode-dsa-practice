53d666944613d0e4b9ead604725a0c16a3a5ed86
Disha Shetty <125394382+dshetty3@users.noreply.github.com>
2025-06-01 13:00:08 -0500

    Time: 4737 ms (20%), Space: 44.3 MB (0%) - LeetHub

---
+++ 2929-distribute-candies-among-children-ii/2929-distribute-candies-among-children-ii.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def distributeCandies(self, n, limit):
+        """
+        :type n: int
+        :type limit: int
+        :rtype: int
+        """
+
+        res = 0
+        for i in range(min(limit, n) + 1):
+            if n - i > 2 * limit:
+                continue
+            res += min(n - i, limit) - max(0, n - i - limit) + 1
+
+        return res
+        
