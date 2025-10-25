e79beb34f7b24d9f203e894555210047eda73ae4
Disha Shetty <125394382+dshetty3@users.noreply.github.com>
2025-10-25 13:31:02 +0530

    Time: 3 ms (46.26%), Space: 12.5 MB (57.82%) - LeetHub

---
+++ 1716-calculate-money-in-leetcode-bank/1716-calculate-money-in-leetcode-bank.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def totalMoney(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+
+        res = 0
+        mon = 1
+
+        while n > 0:
+            for day in range(min(n, 7)):
+                res += mon + day
+            
+            n -= 7
+            mon += 1
+            
+        return res
+        
